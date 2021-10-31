69580ed5d00c6782462ac3e08f520b65c94ad990
Ion Scripnic <asdfree03@gmail.com>
2021-10-31 20:28:19 +0200

    added tema 6

---
+++ Tema 6 - Arrays, collections, generics/ConsoleApp1/GenericArray.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class GenericArray<T>
+    {
+        public const int MaxSize = 50;
+        public T[] Items;
+        public GenericArray()
+        {
+            Items = new T[MaxSize];
+        }
+        public T GetItem(int index)
+        {
+            if (index < MaxSize)
+                return Items[index];
+            else throw new IndexOutOfRangeException();
+        }
+        public void SetItem(T item, int index)
+        {
+            if (index < MaxSize && index >= 0) Items[index] = item;
+            else throw new IndexOutOfRangeException();
+        }
+        public void SwapItemsByIndex(int index1, int index2)
+        {
+            if (index1 < MaxSize && index1 >= 0 && index2 < MaxSize && index2 >= 0)
+            {
+                T aux = Items[index1];
+                Items[index1] = Items[index2];
+                Items[index2] = aux;
+            }
+            else throw new IndexOutOfRangeException();
+         }
+
+        
+        public int find(T item)
+        {
+            for(int i = 0; i < MaxSize; i++)
+            {
+                if (Items[i] != null && Items[i].ToString() == item.ToString()) return i;
+            }
+            return -1;
+        }
+        public void SwapItems(T item1, T item2)
+        {
+            int index1 = find(item1);
+            int index2 = find(item2);
+            if (index1 != -1 && index2 != -1)
+            {
+                T aux = Items[index1];
+                Items[index1] = Items[index2];
+                Items[index2] = aux;
+            }
+            else throw new ArgumentException("Item not found");
+        }
+    }
+}
---
+++ Tema 6 - Arrays, collections, generics/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericArray<string> S = new GenericArray<string>();
+            S.SetItem("asd", 1);
+            Console.WriteLine(S.GetItem(1));
+            S.SetItem("fgh", 2);
+            S.SwapItemsByIndex(1, 2);
+            Console.WriteLine(S.GetItem(1));
+            S.SwapItems("asd", "fgh");
+            Console.WriteLine(S.GetItem(1));
+            
+        }
+    }
+}
