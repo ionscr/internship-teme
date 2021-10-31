e68ef45461249f1fe52240af58a97728af677e43
Ion Scripnic <asdfree03@gmail.com>
2021-10-31 14:03:09 +0200

    first commit

---
+++ tema c# basics/csbasics.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp1
+{
+    public abstract class shape
+    {
+        public string Name { set; get; }
+        public abstract double area();
+
+    }
+    public class Square: shape
+    {
+        protected double l { set; get; }
+        public override double area()
+        {
+            return this.l * this.l;
+        }
+    }
+    public class Rectangle: square, IEnumerable, ICloneable
+    {
+        private double L { set; get; }
+        public override double area()
+        {
+            return this.l * this.L;
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)GetEnumerator();
+        }
+
+        public rectangleEnum GetEnumerator()
+        {
+            return new rectangleEnum();
+        }
+
+        public object Clone()
+        {
+            rectangle r1 = this;
+            return r1;
+        }
+    }
+    public class RectangleEnum: IEnumerator
+    {
+        private int position = -1;
+
+        object IEnumerator.Current { get { return null; } }
+
+        bool IEnumerator.MoveNext()
+        {
+            position++;
+            return true;
+        }
+
+        void IEnumerator.Reset()
+        {
+            position = -1;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ tema exceptions/ConsoleApp1/exceptions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class CustomException : Exception
+    {
+        public CustomException() {}
+        public CustomException(string message): base(message) { }
+    }
+    class Program
+    {
+        public double div(double x1, double x2)
+        {
+            try { 
+                if (x1 == 0) throw new CustomException("Impartirea din 0")  ; 
+                return x1 / x2;
+            }
+            catch(CustomException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine(e.ToString());
+                throw;
+
+            }
+#if DEBUG
+            finally
+            {
+
+                Console.WriteLine("We are in finally!");
+            }
+        }
+#endif
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
