34fb8da89ee9b7269e55480092044a3b573a80c2
Ion Scripnic <asdfree03@gmail.com>
2021-10-31 22:01:06 +0200

    added tema 7

---
+++ Tema 7 - LINQ/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s1 = new Student { FirstName= "Andreo", LastName= "Keo", Age= 18, Country= "Romania"};
+            var s2 = new Student { FirstName = "Opiod", LastName = "Iosde", Age = 28, Country = "Russia" };
+            var s3 = new Student { FirstName = "Oklio", LastName = "Werf", Age = 18, Country = "Romania" };
+            var students = new List<Student> { s1,s2,s3};
+            var filteredByCountry = from s in students
+                                    where s.Country == "Romania"
+                                    select s.FirstName + " " + s.LastName;
+            foreach (var fs in filteredByCountry) Console.WriteLine(fs.ToString());
+            Console.WriteLine();
+            var groupByAge = from s in students
+                             orderby s.Age descending
+                             group s by s.Age into sgroup
+                             select sgroup;
+                             
+                             
+            foreach (var fs in groupByAge)
+            {
+                Console.WriteLine(fs.Key + " " + fs.Count());
+                foreach (var s in fs) Console.WriteLine(s.FirstName + " " + s.LastName);
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ Tema 7 - LINQ/ConsoleApp1/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+
+    }
+}
